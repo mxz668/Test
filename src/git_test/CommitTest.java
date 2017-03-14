7002d11c62c1f68f916c08eb4a1420668d1b345e
MABIAO <mabiao@zillionfortune.com>
2017-03-14 18:06:22 +0800

    test

---
+++ src/git_test/CommitTest.java
@@ -0,0 +1,11 @@
+package git_test;
+
+public class CommitTest {
+	public void test(){
+		
+	}
+	
+	public static void Commit(){
+		
+	}
+}
---
+++ src/git_test/GitTest.java
@@ -0,0 +1,11 @@
+package git_test;
+
+public class GitTest {
+	public void print(){
+		System.out.println();
+	}
+	
+	public static void main(String args[]){
+		System.out.println("123");
+	}
+}
